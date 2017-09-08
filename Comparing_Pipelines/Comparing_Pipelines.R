c65adf9174e3b1f8c240e34fadb29601933acdbf
katemills <klmills@uoregon.edu>
2017-09-08 13:19:22 -0700

    updated script and data to include T1 motion ratings.

--- Comparing_Pipelines/Comparing_Pipelines.R
+++ Comparing_Pipelines/Comparing_Pipelines.R
@@ -18,17 +18,18 @@ lapply(packages, library, character.only = TRUE)
 ## Load your sample ##
 load(paste0(getwd(),"/Comparing_pipelines/exampledata.RData"))
 
+exampledata_comparing$subid<-as.factor(exampledata_comparing$subid)
 
-# Graph Nucleus Accumbens by Age for the cross-sectional pipeline
-Nucleus_Accumbens_Xsectional<-ggplot(data=subcorticaldata_anonymized,
+# Graph PFC Volume by Age for the cross-sectional pipeline
+Prefrontal_Volume_Xsectional<-ggplot(data=exampledata_comparing,
                                      aes(x=age,
-                                         y=nacc_vol_cross))+
+                                         y=prefrontal_vol_cross))+
   xlim(9,23)+
-  ylim(750,2100)+
+  ylim(125000,225000)+
   xlab("Age (years)")+
-  ylab("NAcc volume (cross-sectional)")+
-  geom_line(aes(colour=sex, group=subid),size=.3,alpha=0.3)+
-  geom_point(aes(colour=sex, group=subid),size=2,alpha=0.3)+
+  ylab("PFC volume (cross-sectional)")+
+  geom_line(aes(colour=subid, group=subid),size=.3,alpha=0.3)+
+  geom_point(aes(colour=subid, group=subid),size=2,alpha=0.3)+
   theme_bw() +
   theme_minimal(base_size = 12, base_family = "Arial") +
   theme(axis.line = element_line(colour = "black"),
@@ -39,19 +40,19 @@ Nucleus_Accumbens_Xsectional<-ggplot(data=subcorticaldata_anonymized,
         legend.position="none")
 
 # Take a look
-Nucleus_Accumbens_Xsectional
+Prefrontal_Volume_Xsectional
 
 
-# Graph Nucleus Accumbens by Age for the longitudinal pipeline
-Nucleus_Accumbens_Longitudinal<-ggplot(data=subcorticaldata_anonymized,
-                         aes(x=age,
-                             y=nacc_vol_long))+
+# Graph Prefrontal volume by Age for the longitudinal pipeline
+Prefrontal_Volume_Longitudinal<-ggplot(data=exampledata_comparing,
+                                     aes(x=age,
+                                         y=prefrontal_vol_long))+
   xlim(9,23)+
-  ylim(750,2100)+
+  ylim(125000,225000)+
   xlab("Age (years)")+
-  ylab("NAcc volume (longitudinal)")+
-  geom_line(aes(colour=sex, group=subid),size=.3,alpha=0.3)+
-  geom_point(aes(colour=sex, group=subid),size=2,alpha=0.3)+
+  ylab("PFC volume (longitudinal)")+
+  geom_line(aes(colour=subid, group=subid),size=.3,alpha=0.3)+
+  geom_point(aes(colour=subid, group=subid),size=2,alpha=0.3)+
   theme_bw() +
   theme_minimal(base_size = 12, base_family = "Arial") +
   theme(axis.line = element_line(colour = "black"),
@@ -62,20 +63,20 @@ Nucleus_Accumbens_Longitudinal<-ggplot(data=subcorticaldata_anonymized,
         legend.position="none")
 
 # Take a look
-Nucleus_Accumbens_Longitudinal
+Prefrontal_Volume_Longitudinal
 
 # Subtract difference and graph it
-subcorticaldata_anonymized <- subcorticaldata_anonymized %>%
-  mutate(difference_nacc=(nacc_vol_long-nacc_vol_cross))
+exampledata_comparing <- exampledata_comparing %>%
+  mutate(difference_PFC=(prefrontal_vol_long-prefrontal_vol_cross))
 
-Nucleus_Accumbens_Difference<-ggplot(data=subcorticaldata_anonymized,
+Prefrontal_Volume_Difference<-ggplot(data=exampledata_comparing,
                                        aes(x=age,
-                                           y=difference_nacc))+
+                                           y=difference_PFC))+
   xlim(9,23)+
   xlab("Age (years)")+
   ylab("Difference between volumes (longitudinal - cross-secitonal")+
-  geom_line(aes(colour=sex, group=subid),size=.3,alpha=0.3)+
-  geom_point(aes(colour=sex, group=subid),size=2,alpha=0.3)+
+  geom_line(aes(colour=subid, group=subid),size=.3,alpha=0.3)+
+  geom_point(aes(colour=subid, group=subid),size=2,alpha=0.3)+
   theme_bw() +
   theme_minimal(base_size = 12, base_family = "Arial") +
   theme(axis.line = element_line(colour = "black"),
@@ -86,4 +87,4 @@ Nucleus_Accumbens_Difference<-ggplot(data=subcorticaldata_anonymized,
         legend.position="none")
 
 # Take a look
-Nucleus_Accumbens_Difference
+Prefrontal_Volume_Difference
